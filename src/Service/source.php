2276df6c4a23e66873de7f9140537c63c94f87f2
sergpitk <sergpitk@gmail.com>
2021-11-27 17:39:28 +0200

    lampa-001 call non-member, runtimeException, whole styles

--- src/Service/source.php
+++ src/Service/source.php
@@ -1,6 +1,10 @@
 <?php
 //abstract base class for in-memory representation of various business entities.  The only item
 //we have implemented at this point is InventoryItem (see below).
+namespace App\Service;
+
+use RuntimeException;
+
 abstract class Entity
 {
     static protected $_defaultEntityManager = null;
@@ -18,19 +22,19 @@ abstract class Entity
 
     abstract public function getPrimary();
 
-    //setter for properies and items in the underlying data array
+    //setter for properties and items in the underlying data array
     public function __set($variableName, $value)
     {
         if (array_key_exists($variableName, array_change_key_case($this->getMembers()))) {
             $newData = $this->_data;
             $newData[$variableName] = $value;
-            $this->_update($newData);
+            $this->update($newData);
             $this->_data = $newData;
         } else {
             if (property_exists($this, $variableName)) {
                 $this->$variableName = $value;
             } else {
-                throw new Exception("Set failed. Class " . get_class($this) .
+                throw new RuntimeException("Set failed. Class " . get_class($this) .
                                     " does not have a member named " . $variableName . ".");
             }
         }
@@ -42,7 +46,7 @@ abstract class Entity
         //
     }
 
-    //getter for properies and items in the underlying data array
+    //getter for properties and items in the underlying data array
     public function __get($variableName)
     {
         if (array_key_exists($variableName, array_change_key_case($this->getMembers()))) {
@@ -52,19 +56,19 @@ abstract class Entity
             if (property_exists($this, $variableName)) {
                 return $this->$variableName;
             } else {
-                throw new Exception("Get failed. Class " . get_class($this) .
+                throw new RuntimeException("Get failed. Class " . get_class($this) .
                                     " does not have a member named " . $variableName . ".");
             }
         }
     }
 
-    static public function setDefaultEntityManager($em)
+    public static function setDefaultEntityManager($em): void
     {
         self::$_defaultEntityManager = $em;
     }
 
     //Factory function for making entities.
-    static public function getEntity($entityName, $data, $entityManager = null)
+    public static function getEntity($entityName, $data, $entityManager = null)
     {
         $em = $entityManager === null ? self::$_defaultEntityManager : $entityManager;
         $entity = $em->create($entityName, $data);
@@ -72,15 +76,14 @@ abstract class Entity
         return $entity;
     }
 
-    static public function getDefaultEntityManager()
+    public static function getDefaultEntityManager()
     {
         return self::$_defaultEntityManager;
     }
 
     public function create($entityName, $data)
     {
-        $entity = self::getEntity($entityName, $data);
-        return $entity;
+        return self::getEntity($entityName, $data);
     }
 
     public function read()
@@ -88,25 +91,24 @@ abstract class Entity
         return $this->_data;
     }
 
-    public function update($newData)
+    public function update($newData): void
     {
         $this->_em->update($this, $newData);
         $this->_data = $newData;
     }
 
-    public function delete()
+    public function delete(): void
     {
         $this->_em->delete($this);
     }
 }
 
 //Helper function for printing out error information
-function getLastError()
+function getLastError(): string
 {
     $errorInfo = error_get_last();
-    $errorString = " Error type {$errorInfo['type']}, {$errorInfo['message']} on line {$errorInfo['line']} of " .
+    return " Error type {$errorInfo['type']}, {$errorInfo['message']} on line {$errorInfo['line']} of " .
         "{$errorInfo['file']}. ";
-    return $errorString;
 }
 
 //A super-simple replacement class for a real database, just so we have a place for storing results.
@@ -121,23 +123,23 @@ class DataStore
         $this->_storePath = $storePath;
         if (!file_exists($storePath)) {
             if (!touch($storePath)) {
-                throw new Exception("Could not create data store file $storePath. Details:" . getLastError());
+                throw new RuntimeException("Could not create data store file $storePath. Details:" . getLastError());
             }
             if (!chmod($storePath, 0777)) {
-                throw new Exception("Could not set read/write on data store file $storePath. " .
+                throw new RuntimeException("Could not set read/write on data store file $storePath. " .
                                     "Details:" . getLastError());
             }
         }
         if (!is_readable($storePath) || !is_writable($storePath)) {
-            throw new Exception("Data store file $storePath must be readable/writable. Details:" . getlastError());
+            throw new RuntimeException("Data store file $storePath must be readable/writable. Details:" . getlastError());
         }
         $rawData = file_get_contents($storePath);
 
         if ($rawData === false) {
-            throw new Exception("Read of data store file $storePath failed.  Details:" . getLastError());
+            throw new RuntimeException("Read of data store file $storePath failed.  Details:" . getLastError());
         }
-        if (strlen($rawData > 0)) {
-            $this->_dataStore = unserialize($rawData);
+        if ($rawData !== '') {
+            $this->_dataStore = unserialize($rawData, true);
         } else {
             $this->_dataStore = null;
         }
@@ -157,7 +159,7 @@ class DataStore
     }
 
     //delete an item.
-    public function delete($item, $primary)
+    public function delete($item, $primary): void
     {
         if (isset($this->_dataStore[$item][$primary])) {
             unset($this->_dataStore[$item][$primary]);
@@ -165,16 +167,16 @@ class DataStore
     }
 
     //save everything
-    public function save()
+    public function save(): void
     {
         $result = file_put_contents($this->_storePath, serialize($this->_dataStore));
         if ($result === null) {
-            throw new Exception("Write of data store file $this->_storePath failed.  Details:" . getLastError());
+            throw new RuntimeException("Write of data store file $this->_storePath failed.  Details:" . getLastError());
         }
     }
 
     //Which types of items do we have stored
-    public function getItemTypes()
+    public function getItemTypes(): array
     {
         if (is_null($this->_dataStore)) {
             return array();
@@ -183,23 +185,23 @@ class DataStore
     }
 
     //get keys for an item-type, so we can loop over.
-    public function getItemKeys($itemType)
+    public function getItemKeys($itemType): array
     {
         return array_keys($this->_dataStore[$itemType]);
     }
 }
 
-//This class managed in-memory entities and commmunicates with the storage class (DataStore in our case).
+//This class managed in-memory entities and communicates with the storage class (DataStore in our case).
 class EntityManager
 {
 
-    protected $_entities = array();
+    protected array $_entities = [];
 
-    protected $_entityIdToPrimary = array();
+    protected array $_entityIdToPrimary = [];
 
-    protected $_entityPrimaryToId = array();
+    protected array $_entityPrimaryToId = [];
 
-    protected $_entitySaveList = array();
+    protected array $_entitySaveList = [];
 
     protected $_nextId = null;
 
@@ -214,7 +216,7 @@ class EntityManager
         $itemTypes = $this->_dataStore->getItemTypes();
         foreach ($itemTypes as $itemType)
         {
-            $itemKeys = $this->_dataStore->getItemKeys();
+            $itemKeys = $this->_dataStore->getItemKeys($itemTypes);
             foreach ($itemKeys as $itemKey) {
                 $entity = $this->create($itemType, $this->_dataStore->get($itemType, $itemKey), true);
             }
@@ -274,8 +276,7 @@ class EntityManager
         $this->_entities[$id] = null;
         $primary = $entity->{$entity->getPrimary()};
         $this->_dataStore->delete(get_class($entity),$primary);
-        unset($this->_entityIdToPrimary[$id]);
-        unset($this->_entityPrimaryToId[$primary]);
+        unset($this->_entityIdToPrimary[$id], $this->_entityPrimaryToId[$primary]);
         return null;
     }
 
@@ -290,7 +291,8 @@ class EntityManager
     }
 
     //Update the datastore to update itself and save.
-    public function updateStore() {
+    public function updateStore(): void
+    {
         foreach($this->_entitySaveList as $id) {
             $entity = $this->_entities[$id];
             $this->_dataStore->set(get_class($entity),$entity->{$entity->getPrimary()},$entity->_data);
@@ -328,22 +330,28 @@ class InventoryItem extends Entity
         $this->update($newData);
     }
 
-    public function changeSalePrice($salePrice)
+    public function changeSalePrice($salePrice): void
     {
         $newData = $this->_data;
+        // @todo used void method result
         $newData['salePrice'] = $this->update($newData);
     }
 
-    public function getMembers()
+    public function getMembers(): array
     {
         //These are the field in the underlying data array
-        return array("sku" => 1, "qoh" => 1, "cost" => 1, "salePrice" => 1)    ;
+        return [
+            'sku' => 1,
+            'qoh' => 1,
+            'cost' => 1,
+            'salePrice' => 1
+        ];
     }
 
-    public function getPrimary()
+    public function getPrimary(): string
     {
         //Which field constitutes the primary key in the storage class?
-        return "sku";
+        return 'sku';
     }
 }
 
@@ -355,15 +363,15 @@ function driver()
     //create five new Inventory items
 
     $item1 = Entity::getEntity('InventoryItem',
-                               array('sku' => 'abc-4589', 'qoh' => 0, 'cost' => '5.67', 'salePrice' => '7.27'));
+                               ['sku' => 'abc-4589', 'qoh' => 0, 'cost' => '5.67', 'salePrice' => '7.27']);
     $item2 = Entity::getEntity('InventoryItem',
-                               array('sku' => 'hjg-3821', 'qoh' => 0, 'cost' => '7.89', 'salePrice' => '12.00'));
+                               ['sku' => 'hjg-3821', 'qoh' => 0, 'cost' => '7.89', 'salePrice' => '12.00']);
     $item3 = Entity::getEntity('InventoryItem',
-                               array('sku' => 'xrf-3827', 'qoh' => 0, 'cost' => '15.27', 'salePrice' => '19.99'));
+                               ['sku' => 'xrf-3827', 'qoh' => 0, 'cost' => '15.27', 'salePrice' => '19.99']);
     $item4 = Entity::getEntity('InventoryItem',
-                               array('sku' => 'eer-4521', 'qoh' => 0, 'cost' => '8.45', 'salePrice' => '1.03'));
+                               ['sku' => 'eer-4521', 'qoh' => 0, 'cost' => '8.45', 'salePrice' => '1.03']);
     $item5 = Entity::getEntity('InventoryItem',
-                               array('sku' => 'qws-6783', 'qoh' => 0, 'cost' => '3.00', 'salePrice' => '4.97'));
+                               ['sku' => 'qws-6783', 'qoh' => 0, 'cost' => '3.00', 'salePrice' => '4.97']);
 
     $item1->itemsReceived(4);
     $item2->itemsReceived(2);
